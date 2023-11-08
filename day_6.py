73278d8bd3f007531b44f9e919c52cbe963ee8ea
Felippe A <4drade@gmail.com>
2023-11-08 16:50:38 -0300

    feat: Add new `day_6.py` file

---
+++ day_6.py
@@ -0,0 +1,18 @@
+"""
+enter this site: https://reeborg.ca/reeborg.html and use the code below to solve the problem.
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
+"""
