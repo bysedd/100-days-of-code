fa818976de2e227d1c8dedb334a5f24ded137827
Felippe A. <4drade@gmail.com>
2023-11-25 08:19:19 -0300

    refactor: simplify continue flag logic

--- day_10/main.py
+++ day_10/main.py
@@ -1,19 +1,5 @@
 from art import logo
-from calculator import operations, Number
-
-
-def ask_to_continue(number: Number) -> bool:
-    """
-    This method asks the user if they want to continue using the calculator.
-
-    :param number: The last answer.
-    """
-    return (
-        input(f"Type 'y' to continue calculating with {number}, or 'n' to exit: ")
-        .strip()
-        .lower()
-        == "y"
-    )
+from calculator import operations
 
 
 def calculator() -> None:
@@ -28,10 +14,18 @@ def calculator() -> None:
     while continue_flag:
         operation = input("Pick an operation: ")
         number2 = float(input("What's the next number? "))
-        answer = operations[operation](number1, number2)
-        print(f"{number1} {operation} {number2} = {answer}")
-        number1 = answer
-        continue_flag = ask_to_continue(answer)
+        try:
+            answer = operations[operation](number1, number2)
+            print(f"{number1} {operation} {number2} = {answer}")
+            number1 = answer
+            continue_flag = (
+                input(f"Type 'y' to continue calculating with {number1}, or 'n' to exit: ")
+                .strip()
+                .lower()
+                == "y"
+            )
+        except KeyError:
+            print("Invalid operation. Try again.")
 
 
 calculator()
