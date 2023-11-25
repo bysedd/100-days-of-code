ad258fbe490f96d13d94b3de312bac088c07c177
Felippe A. <4drade@gmail.com>
2023-11-25 08:20:08 -0300

    test: refactor test_operations method

--- day_10/tests.py
+++ day_10/tests.py
@@ -1,36 +1,21 @@
 import unittest
+from random import randint
 
 from calculator import add, subtract, multiply, divide
 
 
 class CalculatorTests(unittest.TestCase):
-    def test_addition_with_positive_numbers(self):
-        self.assertEqual(add(5, 3), 8)
-
-    def test_subtraction_with_positive_numbers(self):
-        self.assertEqual(subtract(5, 3), 2)
-
-    def test_multiplication_with_positive_numbers(self):
-        self.assertEqual(multiply(5, 3), 15)
-
-    def test_division_with_positive_numbers(self):
-        self.assertEqual(divide(6, 3), 2)
-
-    def test_addition_with_negative_numbers(self):
-        self.assertEqual(add(-5, -3), -8)
-
-    def test_subtraction_with_negative_numbers(self):
-        self.assertEqual(subtract(-5, -3), -2)
-
-    def test_multiplication_with_negative_numbers(self):
-        self.assertEqual(multiply(-5, -3), 15)
-
-    def test_division_with_negative_numbers(self):
-        self.assertEqual(divide(-6, -3), 2)
+    def test_operations(self):
+        operations = [add, subtract, multiply, divide]
+        for operation in operations:
+            a = randint(-100, 100)
+            b = randint(-100, 100)
+            expected = operation(a, b)
+            self.assertEqual(operation(a, b), expected)
 
     def test_division_by_zero_raises_exception(self):
         with self.assertRaises(ZeroDivisionError):
-            divide(5, 0)
+            divide(randint(-100, 100), 0)
 
 
 if __name__ == "__main__":
