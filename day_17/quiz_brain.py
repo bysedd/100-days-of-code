771f2217c69774741453a4eeaeacc20cd16797a4
Felippe A. <4drade@gmail.com>
2023-11-30 19:41:41 -0300

    style: fix input formatting

--- day_17/quiz_brain.py
+++ day_17/quiz_brain.py
@@ -10,7 +10,9 @@ class QuizBrain:
     def next_question(self) -> None:
         current_question = self.question_list[self.question_number]
         self.question_number += 1
-        user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False): ")
+        user_answer = input(
+            f"Q.{self.question_number}: {current_question.text} (True/False): "
+        )
         self.check_answer(user_answer, current_question.answer)
 
     def check_answer(self, user_answer: str, answer: str) -> None:
@@ -20,7 +22,9 @@ class QuizBrain:
         else:
             print("That's wrong.")
         print(f"The correct answer was: {answer}.")
-        print(f"Your current score is: {self.__score}/{self.question_number}", end="\n\n")
+        print(
+            f"Your current score is: {self.__score}/{self.question_number}", end="\n\n"
+        )
 
     def still_has_questions(self) -> bool:
         return self.question_number < len(self.question_list)
